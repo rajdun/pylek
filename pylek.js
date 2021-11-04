5f1ea333a2d9b0b7ad9a6c83a0c103b63bc7361d
rajdun <71730219+rajdun@users.noreply.github.com>
2021-11-04 22:02:54 +0100

    inti

---
+++ pylek.js
@@ -0,0 +1,44 @@
+let movingBalls = [];
+let balls = [];
+
+const HEIGHT = 800;
+const WIDTH = 800;
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+
+    balls.push(new Ball());
+
+    for (let index = 0; index < 1000; index++) {
+        const element = new MovingBall(random(0, WIDTH), random(0, HEIGHT), index, balls[0]);
+        movingBalls.push(element);
+    }
+    
+}
+
+function draw() {
+    background(220);
+
+    movingBalls.forEach(ball => {
+        ball.draw();
+    })
+}
+
+class Ball {
+    constructor() {
+        this.img = loadImage("/ball.png");
+    }
+}
+
+class MovingBall {
+    constructor(x, y, i, ball) {
+        this.name = "Testowa piłka " + i;
+        this.x = x;
+        this.y = y;
+        this.ball = ball;
+    }
+
+    draw() {
+        image(this.ball.img, this.x, this.y, 25, 25);
+    }
+}
---
+++ standard.js
@@ -0,0 +1,34 @@
+let balls = [];
+
+const HEIGHT = 800;
+const WIDTH = 800;
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+    for (let index = 0; index < 100; index++) {
+        const element = new Ball(random(0, WIDTH), random(0, HEIGHT), index);
+        balls.push(element);
+    }
+}
+
+function draw() {
+    background(220);
+
+    balls.forEach(ball => {
+        ball.draw();
+    })
+}
+
+
+class Ball {
+    constructor(x, y, i) {
+        this.img = loadImage("/ball.png");
+        this.name = "Testowa piłka " + i;
+        this.x = x;
+        this.y = y;
+    }
+
+    draw() {
+        image(this.img, this.x, this.y, 25, 25);
+    }
+}
